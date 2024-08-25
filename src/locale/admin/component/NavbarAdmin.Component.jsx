4a30c8eaee26b8d6edc80e523ffc34a309f4cc1a
Admin <trinhphunghongphuc15012000@gmail.com>
2024-08-25 17:22:02 +0700

    update nav admin

--- src/locale/admin/component/NavbarAdmin.Component.jsx
+++ src/locale/admin/component/NavbarAdmin.Component.jsx
@@ -21,7 +21,7 @@ const NavbarAdminComponent = () => {
       </div>
       <nav className="flex flex-col space-y-2 mt-6 px-4">
         <Link
-          to="/admin/account"
+          to="/api/account"
           className="flex items-center p-2 hover:bg-gray-800 rounded transition-all duration-200"
         >
           <FontAwesomeIcon icon={faTachometer} className="text-primary mr-3" />
@@ -35,98 +35,60 @@ const NavbarAdminComponent = () => {
           </button>
           <div className="absolute left-0 top-full mt-2 hidden group-hover:block bg-gray-800 rounded shadow-lg z-10">
             <Link
-              to="/admin/game"
+              to="/api/game"
               className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
             >
               Game
             </Link>
             <Link
-              to="/admin/gametypes"
+              to="/api/gametypes"
               className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
             >
               GameTypes
             </Link>
             <Link
-              to="/admin/gamesystem"
+              to="/api/gamesystem"
               className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
             >
               GameSystem
             </Link>
             <Link
-              to="/admin/rankaccount"
+              to="/api/rankaccount"
               className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
             >
               RankAccount
             </Link>
             <Link
-              to="/admin/order"
+              to="/api/order"
               className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
             >
               Order
             </Link>
           </div>
         </div>
+       
         <Link
-          to="/admin/widget"
-          className="flex items-center p-2 hover:bg-gray-800 rounded transition-all duration-200"
-        >
-          <FontAwesomeIcon icon={faTh} className="text-primary mr-3" />
-          <span>Widget</span>
-        </Link>
-        <Link
-          to="/admin/group"
+          to="/api/group"
           className="flex items-center p-2 hover:bg-gray-800 rounded transition-all duration-200"
         >
           <FontAwesomeIcon icon={faKeyboard} className="text-primary mr-3" />
           <span>Group</span>
         </Link>
         <Link
-          to="/admin/keycodes"
+          to="/api/keycodes"
           className="flex items-center p-2 hover:bg-gray-800 rounded transition-all duration-200"
         >
           <FontAwesomeIcon icon={faTable} className="text-primary mr-3" />
           <span>Keycodes</span>
         </Link>
         <Link
-          to="/admin/chart"
+          to="/api/chart"
           className="flex items-center p-2 hover:bg-gray-800 rounded transition-all duration-200"
         >
           <FontAwesomeIcon icon={faChartBar} className="text-primary mr-3" />
           <span>Charts</span>
         </Link>
-        <div className="relative group">
-          <button className="flex items-center p-2 w-full hover:bg-gray-800 rounded transition-all duration-200 focus:outline-none">
-            <FontAwesomeIcon icon={faFile} className="text-primary mr-3" />
-            <span>Pages</span>
-            <i className="ml-auto fa fa-caret-down"></i>
-          </button>
-          <div className="absolute left-0 top-full mt-2 hidden group-hover:block bg-gray-800 rounded shadow-lg z-10">
-            <Link
-              to="/signup"
-              className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
-            >
-              Sign In
-            </Link>
-            <Link
-              to="/"
-              className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
-            >
-              Sign Up
-            </Link>
-            <Link
-              to="/404"
-              className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
-            >
-              404 Error
-            </Link>
-            <Link
-              to="/blank"
-              className="block px-4 py-2 hover:bg-gray-700 transition-all duration-200"
-            >
-              Blank Page
-            </Link>
-          </div>
-        </div>
+
       </nav>
     </div>
   );
